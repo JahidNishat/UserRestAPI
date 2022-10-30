1c1f96fb5ae3f0137a0c555ce5464cd4d349f59f
1610036@student.ruet.ac.bd <1610036@student.ruet.ac.bd>
2022-10-30 10:12:39 +0600

    User View Model Created

---
+++ UserAPI/UserAPI/Model/ViewUserModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserAPI.Model
+{
+    public class ViewUserModel
+    {
+        [Key]
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+}
